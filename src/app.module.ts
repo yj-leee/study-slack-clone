3ec347b21085905b77ad94a477384f1068318477
yj-leee <yj.leee1010@gmail.com>
2022-03-26 23:13:46 +0900

    add: users

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { ChannelsModule } from './channels/channels.module';
+import { DmsModule } from './dms/dms.module';
+
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
+  controllers: [AppController],
+  providers: [AppService, ConfigService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+    }
+}
