216e3c0d9b4a34a3166e8d03e6e31d446fada55b
yj-leee <yj.leee1010@gmail.com>
2022-03-30 08:39:54 +0900

    add: entities

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,28 +1,39 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
+import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
+import { UserDto } from 'src/common/user.dto';
 import { JoinRequestDto } from 'src/users/dto/join.request.dto';
 import { UsersService } from 'src/users/users.service';
 
+@UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
     constructor(private usersService: UsersService) {
-        
     }
+    @ApiResponse({
+        type: UserDto,
+    })
     @ApiOperation({summary:'내 정보 조회'})
     @Get()
-    getUesrs(@Req() req) {
-        return req.user;
+    getUesrs(@User() user) {
+        return user;
     }
     @ApiOperation({summary:'회원가입'})
     @Post()
     postUsers(@Body() data: JoinRequestDto) {
         this.usersService.postUsers(data.email, data.nickname, data.password);
     }
+    @ApiResponse({
+        status: 200,
+        description: '성공',
+        type: UserDto,
+    })
     @ApiOperation({summary:'로그인'})
     @Post('login')
-    logIn(@Req() req) {
-        return req.user;
+    logIn(@User() user) {
+        return user;
     }
     @ApiOperation({summary:'로그아웃'})
     @Post('logout')
