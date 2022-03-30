f7f40e439703719521cf97068332258cf3311f67
yj-leee <yj.leee1010@gmail.com>
2022-03-31 07:09:51 +0900

    add: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,9 +8,12 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { ChannelsModule } from './channels/channels.module';
 import { DmsModule } from './dms/dms.module';
+import * as ormconfig from 'ormconfig';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, WorkspacesModule, ChannelsModule, DmsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, WorkspacesModule, ChannelsModule, 
+    DmsModule,
+    TypeOrmModule.forRoot(ormconfig)],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
--- src/entities/Users.ts
+++ src/entities/Users.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -21,12 +22,24 @@ import { Workspaces } from './Workspaces';
 @Index('email', ['email'], { unique: true })
 @Entity({ schema: 'sleact', name: 'users' })
 export class Users {
+  @ApiProperty({
+    example: '1',
+    description: '사용자 아이디'
+  })
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
+  @ApiProperty({
+    example: 'zerohch0@mail.com',
+    description: '사용자 이메일'
+  })
   @Column('varchar', { name: 'email', unique: true, length: 30 })
   email: string;
 
+  @ApiProperty({
+    example: '제로초',
+    description: '닉네임'
+  })
   @Column('varchar', { name: 'nickname', length: 30 })
   nickname: string;
 
